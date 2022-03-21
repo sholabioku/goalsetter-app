b8b9fa2bae10639d6817e90ee83b97ea169ea2b8
sholabioku <bilushonline@gmail.com>
2022-03-21 18:53:43 +0100

    Add functionality to register a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,47 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
+
+const User = require('../models/userModel');
 
 // @desc Register user
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all required fields');
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+
   res.json({ message: 'User registered' });
 });
 
