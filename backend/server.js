998f24ed9282cc768cbdaecaf179102168df6792
sholabioku <bilushonline@gmail.com>
2022-03-21 16:58:35 +0100

    Add database setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -12,4 +16,6 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server started on port ${port}`.yellow.bold)
+);
