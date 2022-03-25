ef348d4ca6946a2fc3887c57035e94c6ad243929
sholabioku <bilushonline@gmail.com>
2022-03-25 15:50:31 +0100

    Add functionality to create a new goal

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { createGoal } from '../features/goals/goalSilce';
 
 function GoalForm() {
   const [text, setText] = useState('');
+
+  const dispatch = useDispatch();
+
   const onSubmit = (e) => {
     e.preventDefault();
+
+    dispatch(createGoal({ text }));
+    setText('');
   };
 
   return (
